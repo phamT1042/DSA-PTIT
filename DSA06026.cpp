86ff0c823fb00f71ebe4847e09119b565993ffdf
VanTienDesktop <pvantien203@gmail.com>
2023-02-06 10:06:52 +0700

    Sắp xếp nổi bọt

---
+++ DSA06026.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int n, flag; cin >> n;
+	int a[n];
+    for (int &x : a) cin >> x;
+	for (int i = 0; i < n; i++) {
+		flag = 0;
+		for (int j = 0; j < n - i - 1; j++) {
+			if (a[j] > a[j + 1]) {
+				flag = 1;
+				swap(a[j], a[j + 1]);
+			}
+		}
+		if (flag == 1) {
+			cout << "Buoc " << i + 1 << ": ";
+			for (int x : a) cout << x << ' ';
+			cout << endl;
+		}
+	}
+	return 0;
+}
