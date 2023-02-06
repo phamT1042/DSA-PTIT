b0825771809352c7a3ecd056779862c4c4d45a64
VanTienDesktop <pvantien203@gmail.com>
2023-02-06 22:05:22 +0700

    Bộ ba số bằng k

---
+++ DSA06036.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+        int n, k; cin >> n >> k;
+        int a[n];
+        for (int &x : a) cin >> x;
+        sort(a, a + n);
+        int flag = 0;
+        for (int i = 0; i < n - 2; i++) {
+            if (a[i] > k) break;
+            for (int j = i + 1; j < n - 1; j++) {
+                if (a[i] + a[j] > k) break;
+                if (binary_search(a + j + 1, a + n, k - a[i] - a[j])) {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if (flag) cout << "YES\n";
+        else cout << "NO\n";
+	}
+	return 0;
+}
