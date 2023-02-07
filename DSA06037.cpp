80dcc88c4d452de74b1f3f472a136475d954b326
VanTienDesktop <pvantien203@gmail.com>
2023-02-07 20:02:10 +0700

    Sắp xếp đoạn con

--- DSA06037.cpp
+++ DSA06037.cpp
@@ -4,28 +4,26 @@ int main() {
 	int t; cin >> t;
 	while (t--) {
         int n; cin >> n;
-        int a[n], Max[n], Min[n];
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-            Max[i] = a[i];
-            Min[i] = a[i];
-        }
+        int a[n], Max[n], Min[n]; //Max[i]: max trong đoạn [a0,..,ai]
+                                  //Min[i]; min trong đoạn [ai,..,a(n-1)]
+        cin >> a[0];
+        Max[0] = a[0];
         for (int i = 1; i < n; i++) {
+            cin >> a[i];
             Max[i] = max(a[i], Max[i - 1]);
+            Min[i] = a[i];
         }
         for (int i = n - 2; i >= 0; i--) {
             Min[i] = min(a[i], Min[i + 1]);
         }
-        int k = 0;
         vector<int> res;
         for (int i = 0; i < n - 1; i++) {
             if (Max[i] <= Min[i + 1]) {
-                k++;
                 res.push_back(i + 1);
             }
         }
-        if (k) {
-            cout << k << endl;
+        if (res.size() != 0) {
+            cout << res.size() << endl;
             for (int x : res) cout << x << ' ';
             cout << endl;
         }
