bf59c09f5aeedc21fe517ebc1771409a676d42fe
VanTienDesktop <pvantien203@gmail.com>
2023-02-07 21:58:09 +0700

    Bộ 3 số có tổng nhỏ hơn K

--- Sort and Search/DSA06032.cpp
+++ Sort and Search/DSA06032.cpp
@@ -3,10 +3,9 @@ using namespace std;
 int main() {
     int t; cin >> t;
     while (t--) {
-        long long n, k; cin >> n >> k;
+        long long n, k, res = 0; cin >> n >> k;
         long long a[n];
         for (long long &x : a) cin >> x;
-        long long res = 0;
         sort(a, a + n);
         for (int i = 0; i < n - 1; i++) {
             if (a[i] > k) break;
