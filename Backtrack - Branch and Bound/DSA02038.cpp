67b249fcf2e6a81eb98ba187a1bacf1b79881496
VanTien <pvantien203@gmail.com>
2023-02-10 13:00:12 +0700

    Dãy con có K phần tử tăng dần

---
+++ Backtrack - Branch and Bound/DSA02038.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, sh[15], k;
+void result(int a[]) {
+    string s = "";
+    for (int i = 1; i < k; i++) {
+        if (a[sh[i] - 1] > a[sh[i + 1] - 1]) return;
+        s += to_string(a[sh[i] - 1]) + ' '; 
+    }
+    s += to_string(a[sh[k] - 1]);
+    cout << s << endl;
+}
+void Try (int i, int a[]) {
+    for (int j = sh[i - 1] + 1; j <= n - k + i; j++) {
+        sh[i] = j;
+        if (i == k) result(a);
+        else Try(i + 1, a);
+    }
+}
+int main() {
+    int t; cin >> t;
+    sh[0] = 0;
+    while (t--) {
+        cin >> n >> k;
+        int a[n];
+        for (int &x : a) cin >> x;
+        sort(a, a + n);
+        Try(1, a);
+    }
+}
