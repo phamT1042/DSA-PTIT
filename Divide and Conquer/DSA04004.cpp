d07232f948c9dd3a9c8a664520858da2f10cd98c
VanTien <pvantien203@gmail.com>
2023-02-17 10:21:46 +0700

    Gấp đôi dãy số

---
+++ Divide and Conquer/DSA04004.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+long long k;
+long long a[51];
+int Try(int n, long long k) {
+    if (k == a[n] / 2 + 1) return n;
+    if (k > a[n] / 2 + 1) return Try (n - 1, k - a[n] / 2 - 1);
+    return Try (n - 1, k);
+}
+int main() {
+    a[1] = 1;
+    for (int i = 2; i <= 50; i++) a[i] = a[i - 1] * 2 + 1; 
+    int t; cin >> t;
+    while (t--) {
+        cin >> n >> k;
+        cout << Try(n, k) << endl;
+    }
+}
