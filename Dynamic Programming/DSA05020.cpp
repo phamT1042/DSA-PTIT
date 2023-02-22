b0452cc1e8297ef6bf79178c3f3121d82d6971af
VanTienDesktop <pvantien203@gmail.com>
2023-02-22 21:12:41 +0700

    Đường đi nhỏ nhất

--- Dynamic Programming/DSA05020.cpp
+++ Dynamic Programming/DSA05020.cpp
@@ -9,16 +9,13 @@ int main() {
             for (int j = 0; j < m; j++) {
                 cin >> a[i][j];
                 dp[i][j] = a[i][j];
+                if (i == 0 && j > 0) dp[i][j] += dp[i][j - 1];
+                if (j == 0 && i > 0) dp[i][j] += dp[i - 1][j];
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) continue;
-                if (i == 0 && j > 0) dp[i][j] += dp[i][j - 1];
-                else if (i > 0 && j == 0) dp[i][j] += dp[i - 1][j];
-                else {
-                    dp[i][j] = min(dp[i][j] + dp[i - 1][j], min(dp[i][j] + dp[i][j - 1], dp[i][j] + dp[i - 1][j - 1]));
-                } 
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = min(dp[i][j] + dp[i - 1][j], min(dp[i][j] + dp[i][j - 1], dp[i][j] + dp[i - 1][j - 1]));
             }
         }
         cout << dp[n - 1][m - 1] << endl;
