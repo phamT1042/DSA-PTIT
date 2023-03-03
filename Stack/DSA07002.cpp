c885e9ccb51e93a9fc755e79e7f86e2d75470ced
VanTien <pvantien203@gmail.com>
2023-03-03 12:16:05 +0700

    Ngăn xếp 2

--- Stack/DSA07002.cpp
+++ Stack/DSA07002.cpp
@@ -5,8 +5,7 @@ int main() {
     cin.ignore();
     stack<string> a;
     while (t--) {
-        string s;
-        getline(cin, s);
+        string s; getline(cin, s);
         if (s[1] == 'U')
             a.push(s.substr(5, s.length() - 5));
         else if (s[1] == 'O') {
