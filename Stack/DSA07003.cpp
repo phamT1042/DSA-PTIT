b144cfc254d3741549c7fb5d43bff29e72e9db0f
VanTien <pvantien203@gmail.com>
2023-03-03 12:53:14 +0700

    Kiểm tra biểu thức số học

---
+++ Stack/DSA07003.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+string check(vector<char> kt) {
+    stack<char> res;
+    for (char x : kt) {
+        if (x != ')') res.push(x); //chưa gặp dấu đóng ngoặc thì cứ push vào stack
+        else {
+            //khi gặp dấu đóng ngoặc, ta chạy về trước cho đến khi gặp dấu mở ngoặc
+            //loại bỏ tất cả các dấu +, -, * và / trên đường tìm kiếm
+            //Sẽ dư dấu ngoặc khi giữa 2 dấu mở và đóng không có dấu phép tính nào (flag = 0)
+            char sign = res.top(); res.pop();
+            int flag = 0;
+            while (sign != '(') {
+                if (sign != '(') flag = 1;
+                sign = res.top(); res.pop();
+            }
+            if (!flag) return "Yes";
+        }  
+    }
+    return "No";
+}
+int main() {
+    int t; cin >> t;
+    cin.ignore();
+    while (t--) {
+        string s; getline(cin, s);
+        vector<char> kt;
+        for (char x : s) {
+            if (x == '+' || x == '-' || x == '*' || x == '/' || x == '(' || x == ')') {
+                kt.push_back(x);
+            }
+        }
+        cout << check(kt) << endl;
+    }
+}
