b5971b0e061c073f79f21c07a05d1131d332bd01
VanTien <pvantien203@gmail.com>
2023-03-03 12:15:50 +0700

    Ngắn xếp 2

---
+++ Stack/DSA07002.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t; cin >> t;
+    cin.ignore();
+    stack<string> a;
+    while (t--) {
+        string s;
+        getline(cin, s);
+        if (s[1] == 'U')
+            a.push(s.substr(5, s.length() - 5));
+        else if (s[1] == 'O') {
+            if (!a.empty()) a.pop();
+        }
+        else {
+            if (a.empty()) cout << "NONE";
+            else {
+                cout << a.top();
+            }
+            cout << "\n";
+        }
+    }
+}
