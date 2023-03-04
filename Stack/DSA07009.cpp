4266cac2c070767980b211795c3c93dab5b58cda
VanTienDesktop <pvantien203@gmail.com>
2023-03-04 22:06:31 +0700

    Biến đổi tiền tố - trung tố

---
+++ Stack/DSA07009.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+        string s; cin >> s;
+        stack<string> res;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                string tmp = "";
+                tmp += s[i];
+                res.push(tmp);
+            }
+            else {
+                string bt1 = res.top(); res.pop();
+                string bt2 = res.top(); res.pop();
+                res.push("(" + bt1 + s[i] + bt2 + ")");
+            }
+        }
+        cout << res.top() << endl;
+	}
+	return 0;
+}
