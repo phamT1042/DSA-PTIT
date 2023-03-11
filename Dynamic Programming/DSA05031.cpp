1f263a1401aeb7c43f59220f625031bdba3ac745
VanTienDesktop <pvantien203@gmail.com>
2023-03-11 09:42:24 +0700

    Tổng bình phương

--- Dynamic Programming/DSA05031.cpp
+++ Dynamic Programming/DSA05031.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 long long dp[10001];
 int main() {
-    long long MOD = 1e9 + 7;
     for (int i = 1; i <= 100; i++) dp[i * i] = 1;
     for (int i = 2; i < 10000; i++) {
         if (dp[i]) continue;
         dp[i] = LONG_LONG_MAX;
         for (int j = 1; j <= i / 2; j++) {
-            dp[i] = min(dp[i], (dp[j] + dp[i - j]) % MOD);
+            dp[i] = min(dp[i], (dp[j] + dp[i - j]));
         }
     }
     int t; cin >> t;
