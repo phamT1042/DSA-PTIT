224ebad37c628a13bbe4a0af1cf613079f2890f7
VanTienDesktop <pvantien203@gmail.com>
2023-03-16 21:31:41 +0700

    Số BDN 1

--- Queue/DSA08007.cpp
+++ Queue/DSA08007.cpp
@@ -4,15 +4,16 @@ using namespace std;
 int main() {
     int t; cin >> t;
     while (t--) {
-        long long n; cin >> n;
+        string n; cin >> n;
         queue<string> q;
         q.push("1");
         int cnt = 0;
-        while (stoll(q.front()) <= n) {
-            cnt++;
-            q.push(q.front() + "0");
-            q.push(q.front() + "1");
-            q.pop();
+        while (1) {
+            string s = q.front(); q.pop();
+            if (s.length() == n.length() && s <= n || s.length() < n.length()) cnt++;
+            else break;
+            q.push(s + "0");
+            q.push(s + "1");
         }
         cout << cnt << endl;
     }
