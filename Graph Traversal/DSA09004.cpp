4b16f6b913f4325a04c830d39801000fd65d4b22
VanTienDesktop <pvantien203@gmail.com>
2023-03-18 22:09:24 +0700

    DFS trên đồ thị vô hướng

---
+++ Graph Traversal/DSA09004.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> ke[1001];
+int visited[1001];
+void DFS (int u) {
+    visited[u] = 1;
+    cout << u << ' ';
+    for (int x : ke[u]) {
+        if (!visited[x]) DFS(x);
+    }
+}
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int v, e, u, x, y; cin >> v >> e >> u;
+        while (e--) {
+            cin >> x >> y;
+            ke[x].push_back(y);
+            ke[y].push_back(x);
+        }
+        DFS(u);
+        cout << endl;
+        for (int i = 1; i < 1001; i++) ke[i].clear();
+        memset(visited, 0, sizeof(visited));
+    }
+    return 0;
+}
