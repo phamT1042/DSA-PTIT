e9c7fc2bac7348ee4c296ed3fef3f95ab6bf41cf
VanTienDesktop <pvantien203@gmail.com>
2023-03-19 22:43:19 +0700

    Kiểm tra đường đi

--- Graph Traversal/DSA09027.cpp
+++ Graph Traversal/DSA09027.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 vector<int> ke[1001];
 int visited[1001];
-int flag;
-void BFS (int s, int t) {
+string BFS (int s, int t) {
     queue<int> q;
     q.push(s);
     visited[s] = 1;
@@ -11,15 +10,13 @@ void BFS (int s, int t) {
         int tmp = q.front(); q.pop();
         for (int x : ke[tmp]) {
             if (!visited[x]) {
+                if (x == t) return "YES\n";
                 q.push(x);
                 visited[x] = 1;
-                if (x == t) {
-                    flag = 1;
-                    return;
-                }
             }
         }
     }
+    return "NO\n";
 }
 int main() {
     int T; cin >> T;
@@ -32,11 +29,8 @@ int main() {
         }
         int q; cin >> q;
         while (q--) {
-            flag = 0;
             int s, t; cin >> s >> t;
-            BFS(s, t);
-            if (flag) cout << "YES\n";
-            else cout << "NO\n";
+            cout << BFS(s, t);
             memset(visited, 0, sizeof(visited));
         }
         for (int i = 1; i < 1001; i++) ke[i].clear();
