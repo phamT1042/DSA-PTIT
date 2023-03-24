3e0502ccd51d0effcabf8758e2b9dd009c134381
VanTienDesktop <pvantien203@gmail.com>
2023-03-24 22:19:49 +0700

    Đường đi Hamilton

---
+++ Graph Traversal/DSA09029.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[12], visited[12];
+vector<int> ke[12];
+int v;
+bool DFS_Hamilton_path(int i) {
+    for (int j : ke[a[i - 1]]) {
+        if (!visited[j]) {
+            a[i] = j;
+            visited[j] = 1;
+            if (i == v) return true;
+            else if (DFS_Hamilton_path(i + 1)) return true;
+            visited[j] = 0;
+        }
+    }
+    return false;
+}
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int e; cin >> v >> e;
+        while (e--) {
+            int x, y; cin >> x >> y;
+            ke[x].push_back(y);
+            ke[y].push_back(x);
+        }
+        int flag = 0;
+        for (int i = 1; i <= v; i++) {
+            a[1] = i;
+            memset(visited, 0, sizeof(visited));
+            visited[i] = 1;
+            if (DFS_Hamilton_path(2)) {
+                cout << "1\n";
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) cout << "0\n";
+        for (int i = 1; i <= v; i++) ke[i].clear();
+        memset(visited, 0, sizeof(visited));
+    }
+}
