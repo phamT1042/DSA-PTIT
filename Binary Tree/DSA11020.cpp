1763e940e4cef62c5ea6ddbb2df4b212d1f5725a
VanTien <pvantien203@gmail.com>
2023-05-05 14:47:15 +0700

    Kiểm tra cây nhị phân tìm kiếm

--- Binary Tree/DSA11020.cpp
+++ Binary Tree/DSA11020.cpp
@@ -3,7 +3,15 @@ using namespace std;
 int main() {
     int t; cin >> t;
     while (t--) {
-        int n; cin >> n;
-        
+        int n, flag = 1; cin >> n;
+        int a[n];
+        for (int &x : a) cin >> x;
+        for (int i = 1; i < n; i++) {
+            if (a[i - 1] >= a[i]) {
+                flag = 0;
+                break;
+            }
+        }
+        cout << flag << '\n';
     }
 }
