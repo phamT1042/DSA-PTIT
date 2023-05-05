f157f557571c34ab64ce9d4624449c1274649eae
VanTien <pvantien203@gmail.com>
2023-05-05 14:49:50 +0700

    Cây nhị phân tìm kiếm cân bằng 1

---
+++ Binary Tree/DSA11024.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        int a[n];
+        for (int &x : a) cin >> x;
+        sort(a, a + n);
+        cout << a[(n - 1) / 2] << '\n';
+    }
+}
