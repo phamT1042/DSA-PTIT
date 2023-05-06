3604829dff48347df33c093d6f20eb5ae952ac88
VanTienDesktop <pvantien203@gmail.com>
2023-05-06 08:50:11 +0700

    Loại bỏ dấu ngoặc

--- Backtrack - Branch and Bound/DSA02015.cpp
+++ Backtrack - Branch and Bound/DSA02015.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool check (string s) {
-    if (s == "") return false;
-    if (s.length() == 1 && s[0] != '(' && s[0] != ')') return false;
+    if (s == "") return false;  //rỗng
+    if (s.length() == 1 && s[0] != '(' && s[0] != ')') return false; //chỉ 1 kí tự và nó không phải ngoặc
     int cnt = 0;
     for (char x : s) {
         if (x == '(') cnt++;
@@ -15,11 +15,11 @@ int main() {
     int t; cin >> t;
     while (t--) {
         string s; cin >> s;
-        queue<string> del;
-        map<string, int> xetDayNgoac;
-        set<string> res;
+        queue<string> del;   //Lưu các cách xoá ngoặc
+        map<string, int> xetDayNgoac; //Kiểm tra cách xoá ngoặc này liệu đã tồn tại ?
+        set<string> res;   
         del.push({s, 0});
-        int loaiBo = 0;
+        int loaiBo = 0;    //lưu số phép loại bỏ ít nhất để được 1 dãy ngoặc đúng
         while (del.size()) {
             string tmp = del.front(); del.pop();
             if (check(tmp)) {
