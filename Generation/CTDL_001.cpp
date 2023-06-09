ff5976f3a92066be8cde6591773d1a10912b05d7
VanTienDesktop <pvantien203@gmail.com>
2023-06-09 21:01:15 +0700

    Thuật toán sinh

--- Generation/CTDL_001.cpp
+++ Generation/CTDL_001.cpp
@@ -3,14 +3,13 @@ using namespace std;
 int x[1001], m, n;
 void result () {
     for (int i = 1; i <= m; i++) cout << x[i] << ' ';
-    if (n % 2 == 0) {
-        for (int i = m; i >= 1; i--) cout << x[i] << ' ';
-        cout << endl;
-    }
-    else {
-        for (int i = m - 1; i >= 1; i--) cout << x[i] << ' ';
-        cout << endl;
-    }
+    int mid;
+    if (n % 2 == 0) 
+        mid = m;
+    else 
+        mid = m - 1;
+    for (int i = mid; i >= 1; i--) cout << x[i] << ' ';
+    cout << '\n';
 }
 void Try (int i) {
     for (int j = 0; j <= 1; j++) {
@@ -21,9 +20,8 @@ void Try (int i) {
 }
 int main() {
     cin >> n;
-    if (n == 1) {
+    if (n == 1) 
         cout << "0\n1\n";
-    }
     else {
         m = n / 2;
         if (n % 2 == 1) m++;
