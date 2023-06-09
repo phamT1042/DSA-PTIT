e068af5976c6e7453c2831dbe80061a71e45e59a
VanTienDesktop <pvantien203@gmail.com>
2023-06-09 21:07:28 +0700

    Xâu nhị phân kế tiếp

--- Generation/DSA01001.cpp
+++ Generation/DSA01001.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-void sinh (string x) {
-    int i = x.length() - 1;
-    while (i >= 0 && x[i] == '1') x[i--] = '0';
-    if (i != -1) x[i] = '1';
-    cout << x << endl;
-}
 int main() {
     int t; cin >> t;
     while (t--) {
         string x; cin >> x;
-        sinh(x);
+        int i = x.length() - 1;
+        while (i >= 0 && x[i] == '1') x[i--] = '0';
+        if (i != -1) x[i] = '1';
+        cout << x << '\n';
     }
     return 0;
 }
