065e5b5a15068ed1fc36ec14b240b3625d94f152
VanTienDesktop <pvantien203@gmail.com>
2023-06-09 21:13:32 +0700

    Hoán vị kế tiếp

--- Generation/DSA01003.cpp
+++ Generation/DSA01003.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 void sinh (int a[], int n) {
     int i = n - 2;
-    while (i >= 0 && a[i] >= a[i + 1]) i--;
-    if (i == -1) {
+    while (i >= 0 && a[i] > a[i + 1]) i--;
+    if (i == -1) 
         for (int j = 1; j <= n; j++) cout << j << ' ';
-    }
     else {
         int k = n - 1;
-        while (a[i] >= a[k]) k--;
+        while (a[i] > a[k]) k--;
         swap(a[i], a[k]);
         int l = i + 1, r = n - 1;
-        while (l < r) {
+        while (l < r) 
             swap(a[l++], a[r--]);
-        }
         for (int j = 0; j < n; j++) cout << a[j] << ' ';
     }
-    cout << endl;
+    cout << '\n';
 }
 int main () {
     int t; cin >> t;
