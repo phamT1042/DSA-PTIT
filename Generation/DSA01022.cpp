abba5120cb273ad902717951e9af73c515c79818
VanTienDesktop <pvantien203@gmail.com>
2023-06-09 21:36:48 +0700

    Số thứ tự hoán vị

--- Generation/DSA01022.cpp
+++ Generation/DSA01022.cpp
@@ -5,27 +5,24 @@ int ck[11];
 void reset (int n) {
     for (int i = 0; i < n; i++) ck[i] = i + 1;
 }
+bool check (int a[], int n) {
+    for (int i = 0; i < n; i++) 
+        if (a[i] != ck[i]) return false;
+    return true;
+}
 void sinhHV (int a[], int n) {
-    int check = 1;
-    for (int i = 0; i < n; i++) {
-        if (a[i] != ck[i]) {
-            check = 0;
-            break;
-        }
-    }
-    if (check) {
+    if (check(a, n)) {
         flag = 1;
         return;
     }
     int i = n - 2;
-    while (i >= 0 && ck[i] >= ck[i + 1]) i--;
+    while (i >= 0 && ck[i] > ck[i + 1]) i--;
     int k = n - 1;
-    while (ck[i] >= ck[k]) k--;
+    while (ck[i] > ck[k]) k--;
     swap(ck[i], ck[k]);
     int l = i + 1, r = n - 1;
-    while (l < r) {
+    while (l < r) 
         swap(ck[l++], ck[r--]);
-    }
 }
 int main () {
     int t; cin >> t;
