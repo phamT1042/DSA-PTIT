55894a7f17cd6149182108cae079d8350b90b269
VanTienDesktop <pvantien203@gmail.com>
2023-06-10 09:27:43 +0700

    Mã Gray 3

--- Generation/DSA01017.cpp
+++ Generation/DSA01017.cpp
@@ -4,16 +4,11 @@ int main() {
     int t; cin >> t;
     while (t--) {
         string bit; cin >> bit;
-        char gray[bit.length()];
-        for (int i = 1; i < bit.length(); i++) {
-            if (bit[i] != bit[i - 1]) gray[i] = '1';
-            else gray[i] = '0';
-        }
-        gray[0] = bit[0];
-        for (int i = 0; i < bit.length(); i++) {
-            cout << gray[i];
-        }
-        cout << endl;
+        cout << bit[0];
+        for (int i = 1; i < bit.length(); i++) 
+            if (bit[i] != bit[i - 1]) cout << 1;
+            else cout << 0;
+        cout << '\n';
     }
     return 0;
 }
