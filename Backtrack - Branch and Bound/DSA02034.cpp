7c8eb173266d92d9d5a6839481c69ce31007241a
VanTienDesktop <pvantien203@gmail.com>
2023-06-10 16:17:02 +0700

    Trò chơi với các con số

--- Backtrack - Branch and Bound/DSA02034.cpp
+++ Backtrack - Branch and Bound/DSA02034.cpp
@@ -2,24 +2,16 @@
 using namespace std;
 int a[11], n;
 bool used[11];
-bool check() {
-    for (int i = 0; i < n - 1; i++) {
-        if (abs(a[i] - a[i + 1]) == 1) return false;
-    }
-    return true;
-}
 void result() {
-    if (check()) {
-        for (int i = 0; i < n; i++) cout << a[i];
-        cout << endl;
-    }
+    for (int i = 1; i <= n; i++) cout << a[i];
+    cout << '\n';
 }
 void Try (int i) {
     for (int j = 1; j <= n; j++) {
-        if (!used[j]) {
+        if (!used[j] && abs(a[i - 1] - j) != 1) {
             a[i] = j;
             used[j] = true;
-            if (i == n - 1) result();
+            if (i == n) result();
             else Try(i + 1);
             used[j] = false;
         }
@@ -27,8 +19,9 @@ void Try (int i) {
 }
 int main() {
     int t; cin >> t;
+    a[0] = 15;
     while (t--) {
         cin >> n;
-        Try(0);
+        Try(1);
     }
 }
