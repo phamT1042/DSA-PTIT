4bc9f80d77dd2ba296d6afb162aa67470e9c7aa5
VanTienDesktop <pvantien203@gmail.com>
2023-06-10 10:24:26 +0700

    Dãy số 1

--- Backtrack - Branch and Bound/DSA02001.cpp
+++ Backtrack - Branch and Bound/DSA02001.cpp
@@ -1,27 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-void result (int a[], int n) {
-    cout << "[";
-    for (int i = 0; i < n; i++) {
-        cout << a[i];
-        if (i < n - 1) cout << ' ';
-    }
-    cout << "]\n";
-}
-void Try (int a[], int n) {
-    if (n == 0) return;
-    result(a, n);
-    for (int i = 0; i < n - 1; i++) {
-        a[i] = a[i] + a[i + 1];
-    }
-    Try(a, n - 1);
-}
 int main() {
     int t; cin >> t;
     while (t--) {
         int n; cin >> n;
         int a[n];
-        for (int &x : a) cin >> x;
-        Try(a, n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        cout << "[";
+        for (int i = 0; i < n; i++) {
+            cout << a[i];
+            if (i < n - 1) cout << ' ';
+        }
+        cout << "]\n";
+        for (int i = n - 1; i >= 1; i--) {
+            cout << '[';
+            for (int j = 0; j < i; j++) {
+                a[j] += a[j + 1];
+                cout << a[j];
+                if (j < i - 1) cout << ' ';
+            }
+            cout << ']' << '\n';
+        }
     }
 }
