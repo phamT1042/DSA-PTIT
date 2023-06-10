c666a3626b3e0a8023066d0600c477c27466c357
VanTienDesktop <pvantien203@gmail.com>
2023-06-10 22:02:33 +0700

    Sắp xếp xen kẽ

--- Sort and Search/DSA06001.cpp
+++ Sort and Search/DSA06001.cpp
@@ -1,33 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-void quickSort(int a[], int left, int right) {
-    int x = a[(left + right) / 2];
-    int i = left, j = right;
-    do {
-        while (a[i] < x) i++;
-        while (a[j] > x) j--;
-        if (i <= j) {
-            swap(a[i++], a[j--]);
-        }
-    } while (i < j);
-    if (left < j) quickSort(a, left, j);
-    if (i < right) quickSort(a, i, right);
-}
 int main() {
     int t; cin >> t;
     while (t--) {
         int n; cin >> n;
         int a[n];
         for (int &x : a) cin >> x;
-        quickSort(a, 0, n - 1);
-        vector<int> s (a, a + n / 2);
-        vector<int> e (a + n / 2, a + n);
-        reverse(e.begin(), e.end());
-        int j = 0, k = 0;
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) cout << e[j++] << ' ';
-            else cout << s[k++] << ' ';
+        sort(a, a + n);
+        int l = 0, r = n - 1, i = 0;
+        while (l <= r) {
+            if (!i) {
+                cout << a[r--] << ' '; i = 1; 
+            }
+            else {
+                cout << a[l++] << ' '; i = 0;
+            }
         }
-        cout << endl;
+        cout << '\n';
     }
 }
