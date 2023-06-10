36584b8af4ea837d1c5ba93eeb46b5ebd1528b33
VanTienDesktop <pvantien203@gmail.com>
2023-06-10 21:44:03 +0700

    Ký tự đặc biệt

--- Backtrack - Branch and Bound/DSA02021.cpp
+++ Backtrack - Branch and Bound/DSA02021.cpp
@@ -6,13 +6,10 @@ int Try (long long i, long long n) {
         if (i == 0) return s.length() - 1;
         return i - 1;
     }
-    //Nếu vị trí i là n => là vị trí n/2 của xâu đảo với xâu n/2
-    //=> là vị trí n/2 - 1 của xâu n/2
-    if (i == n) return Try (n / 2 - 1, n / 2);
-    //Nếu vị trí i nằm trong khoảng n đến n/2 => i nằm ở vị trí i - n/2
+    //Nếu vị trí i nằm trong khoảng n/2 đến n => i nằm ở vị trí i - n/2
     //của xâu đảo với xâu n/2 => vị trí i - n/2 - 1 của xâu n/2
-    if (i < n && i > n / 2) return Try (i - n / 2 - 1, n / 2);
-    //Nếu vị trí i nhỏ hơn n / 2 => giảm n đến TH trong khoảng (n/2, n)
+    if (i <= n && i > n / 2) return Try (i - n / 2 - 1, n / 2);
+    //Nếu vị trí i nhỏ hơn n / 2 
     return Try (i, n / 2);
 }
 int main() {
