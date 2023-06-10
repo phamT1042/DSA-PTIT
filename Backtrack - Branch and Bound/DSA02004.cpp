20e6568a455ae5a3e59ef3a87ba75a55601c1f88
VanTienDesktop <pvantien203@gmail.com>
2023-06-10 16:23:04 +0700

    Di chuyển trong mê cung 2

--- Backtrack - Branch and Bound/DSA02004.cpp
+++ Backtrack - Branch and Bound/DSA02004.cpp
@@ -24,7 +24,7 @@ void Try (int i, int j) {
     for (int l = 0; l < 4; l++) {
         int i1 = i + dx[l];
         int j1 = j + dy[l];
-        if (i1 < n && i1 >= 0 && j1 < n && j1 >= 0 && a[i1][j1] == 1 && visited[i1][j1] == 0) {
+        if (i1 < n && i1 >= 0 && j1 < n && j1 >= 0 && a[i1][j1] && !visited[i1][j1]) {
             visited[i1][j1] = 1;
             go[k] = l;
             k++;
@@ -38,24 +38,21 @@ int main() {
     int t; cin >> t;
     while (t--) {
         cin >> n;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++) 
                 cin >> a[i][j];
-                visited[i][j] = 0;
-            }
-        }
-        if (a[0][0] == 1 && a[n - 1][n - 1] == 1) {
+        if (a[0][0] && a[n - 1][n - 1]) {
             k = 0;
             visited[0][0] = 1;
             Try(0, 0);
-            if (res.size() == 0) cout << "-1";
+            if (res.empty()) cout << -1;
             else {
                 sort(res.begin(), res.end());
                 for (string x : res) cout << x << ' ';
                 res.clear();
             }
         }
-        else cout << "-1";
+        else cout << -1;
         cout << "\n";
     }
     return 0;
