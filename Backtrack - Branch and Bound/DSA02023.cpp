bcfca59ce5a0535e46432a10b4898e0a336bd791
VanTienDesktop <pvantien203@gmail.com>
2023-06-10 15:38:55 +0700

    Đặt tên

---
+++ Backtrack - Branch and Bound/DSA02023.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<string> name;
+int sh[31];
+int k;
+void result() {
+    for (int i = 1; i <= k; i++) 
+        cout << name[sh[i] - 1] << ' ';
+    cout << '\n';
+}
+void Try (int i) {
+    for (int j = sh[i - 1] + 1; j <= name.size() - k + i; j++) {
+        sh[i] = j;
+        if (i == k) result();
+        else Try(i + 1);
+    }
+}
+int main() {
+    int n; 
+    cin >> n >> k;
+    set<string> nameIN;
+    while (n--) {
+        string s; cin >> s;
+        nameIN.insert(s);
+    }
+    for (string x : nameIN) name.push_back(x);
+    Try(1);
+}
