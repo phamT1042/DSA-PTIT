c503a384417e43ae40cef1a63ecb7dc76151dabc
VanTienDesktop <pvantien203@gmail.com>
2023-06-10 16:57:38 +0700

    Số nguyên tố

--- Backtrack - Branch and Bound/DSA02013.cpp
+++ Backtrack - Branch and Bound/DSA02013.cpp
@@ -1,39 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-int sang[200], n, p, s, sh[11], used[200];
-set<string> res;
+int n, p, s, sh[11], used[200];
+vector<int> nguyenTo;
+vector<vector<int>> res;
 void sangNT() {
-    for (int i = 0; i < 200; i++) sang[i] = true;
-    sang[0] = sang[1] = false;
-    for (int i = 2; i < 14; i++) {
-        if (sang[i]) {
+    int sang[200];
+    for (int i = 0; i < 200; i++) sang[i] = 1;
+    sang[0] = sang[1] = 0;
+    for (int i = 2; i < 14; i++) 
+        if (sang[i]) 
             for (int j = i * i; j < 200; j += i)
-                sang[j] = false;
-        }
-    }
+                sang[j] = 0;
+    for (int i = 2; i < 200; i++)
+        if (sang[i]) nguyenTo.push_back(i);
 }
 void result() {
     int sum = 0;
-    set<int> check;
-    string tmp = "";
+    vector<int> tmp;
     for (int i = 0; i < n; i++) {
         sum += sh[i];
+        tmp.push_back(sh[i]);
         if (sum > s) return;
-        check.insert(sh[i]);
     }
-    if (sum < s) return;
-    for (int x : check) tmp += to_string(x) + ' ';
-    tmp.erase(tmp.length() - 1, 1);
-    res.insert(tmp);
+    if (sum == s) res.push_back(tmp);
 }
-void Try (int i) {
-    for (int j = p + 1; j < s; j++) {
-        if (sang[j] && !used[j]) {
-            used[j] = true;
-            sh[i] = j;
+void Try (int i, int k) {
+    for (int j = k; j < nguyenTo.size(); j++) {
+        if (!used[nguyenTo[j]]) {
+            used[nguyenTo[j]] = 1;
+            sh[i] = nguyenTo[j];
             if (i == n - 1) result();
-            else Try(i + 1);
-            used[j] = false;
+            else Try(i + 1, j + 1);
+            used[nguyenTo[j]] = 0;
         }
     }
 }
@@ -42,10 +40,15 @@ int main() {
     int t; cin >> t;
     while(t--) {
         cin >> n >> p >> s;
-        memset(used, false, sizeof(used));
-        Try(0);
-        cout << res.size() << endl;
-        for (string x : res) cout << x << endl;
+        int i = 0;
+        for (i; i < nguyenTo.size(); i++)
+            if (nguyenTo[i] > p) break;
+        Try(0, i);
+        cout << res.size() << '\n';
+        for (auto it : res) {
+            for (int x : it) cout << x << ' ';
+            cout << '\n';
+        }
         res.clear();
     }
     return 0;
