1dc50ade75208104bb36e23c78ccb33b3c7623db
VanTienDesktop <pvantien203@gmail.com>
2023-06-12 15:18:16 +0700

    Di chuyển trong ma trận

--- Queue/DSA08021.cpp
+++ Queue/DSA08021.cpp
@@ -1,39 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-int d[1001][1001], a[1001][1001];
-int n, m;
-bool solve() {
-    queue<pair<int, int>> q;
-    q.push({0, 0});
+int solve(vector<int> a[], int n, int m) {
+    queue<pair<pair<int, int>, int>> q;
+    q.push({{0, 0}, 0});
     int down, right, i, j;
-    while (!q.empty()) {
+    int vs[n][m];
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++) vs[i][j] = 0;
+    vs[0][0] = 1;
+    while (q.size()) {
         auto it = q.front(); q.pop();
-        i = it.first; j = it.second;  
+        pair<int, int> vt = it.first;
+        int d = it.second;
+        i = vt.first; j = vt.second;
         down = i + a[i][j];
         right = j + a[i][j];
-        if (down < n && !d[down][j]) {
-            d[down][j] = d[i][j] + 1;
-            if (down == n - 1 && j == m - 1) return true;
-            else q.push({down, j});
+        if (down < n && !vs[down][j]) {
+            if (down == n - 1 && j == m - 1) return d + 1;
+            vs[down][j] = 1;
+            q.push({{down, j}, d + 1});
         }
-        if (right < m && !d[i][right]) {
-            d[i][right] = d[i][j] + 1;
-            if (i == n - 1 && right == m - 1) return true;
-            else q.push({i, right});
+        if (right < m && !vs[i][right]) {
+            if (i == n - 1 && right == m - 1) return d + 1;
+            vs[i][right] = 1;
+            q.push({{i, right}, d + 1});
         }
     }
-    return false;
+    return -1;
 }
 int main () {
     int t; cin >> t;
     while (t--) {
+        int n, m, x;
         cin >> n >> m;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++) cin >> a[i][j];
-        memset(d, 0, sizeof(d));
-        if (solve()) cout << d[n - 1][m - 1];
-        else cout << "-1";
-        cout << endl;
+        vector<int> a[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> x; a[i].push_back(x);
+            }
+        }
+        cout << solve(a, n, m) << '\n';
     }
     return 0;
 }
