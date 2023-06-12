29ca18c86c55543ddd80480e2fb0eb15b20a108d
VanTienDesktop <pvantien203@gmail.com>
2023-06-12 21:42:06 +0700

    Kiểm tra đường đi

--- Graph Traversal/DSA09027.cpp
+++ Graph Traversal/DSA09027.cpp
@@ -1,39 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> ke[1001];
-int visited[1001];
-string BFS (int s, int t) {
-    queue<int> q;
-    q.push(s);
-    visited[s] = 1;
-    while (!q.empty()) {
-        int tmp = q.front(); q.pop();
-        for (int x : ke[tmp]) {
-            if (!visited[x]) {
-                if (x == t) return "YES\n";
-                q.push(x);
-                visited[x] = 1;
-            }
-        }
+int parent[1001], sz[1001];
+void Make(int n) {
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+        sz[i] = 1;
     }
-    return "NO\n";
+}
+int Find (int u) {
+    if (parent[u] == u) return u;
+    return parent[u] = Find(parent[u]);
+}
+void Union (int a, int b) {
+    a = Find(a);
+    b = Find(b);
+    if (a == b) return;
+    if (sz[a] < sz[b]) swap(a, b);
+    sz[a] += sz[b];
+    parent[b] = a;
 }
 int main() {
     int T; cin >> T;
     while (T--) {
         int n, m, x, y; cin >> n >> m;
+        Make(n);
         while (m--) {
             cin >> x >> y;
-            ke[x].push_back(y);
-            ke[y].push_back(x);
+            Union(x, y);
         }
         int q; cin >> q;
         while (q--) {
             int s, t; cin >> s >> t;
-            cout << BFS(s, t);
-            memset(visited, 0, sizeof(visited));
+            if (Find(s) == Find(t)) cout << "YES";
+            else cout << "NO";
+            cout << '\n';
         }
-        for (int i = 1; i < 1001; i++) ke[i].clear();
     }
     return 0;
 }
