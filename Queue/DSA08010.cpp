a0aec36baf16bc50d891ddbf55ba40ed7638b3c2
VanTienDesktop <pvantien203@gmail.com>
2023-06-12 15:30:41 +0700

    Biến đổi số tự nhiên

--- Queue/DSA08010.cpp
+++ Queue/DSA08010.cpp
@@ -4,28 +4,25 @@ using namespace std;
 int solve(int n) {
     queue<pair<int, int>> q; 
     q.push({n, 0});
-    set<int> check;
-    check.insert(n);
+    unordered_map<int, int> check;
+    check[n] = 1; 
     while (!q.empty()) {
         int i = q.front().first;
         int j = q.front().second;
         q.pop();
-        if (i - 1 > 0 && check.find(i - 1) == check.end()) {
+        if (i - 1 > 0 && !check[i - 1]) {
             if (i - 1 == 1) return j + 1;
             else {
                 q.push({i - 1, j + 1});
-                check.insert(i - 1);
+                check[i - 1] = 1;
             }
         }
         for (int k = 2; k <= sqrt(i); k++) {
             if (i % k == 0) {
-                int select = max(k, i / k);
-                if (check.find(select) == check.end()) {
-                    if (select == 1) return j + 1;
-                    else {
-                        q.push({select, j + 1});
-                        check.insert(select);
-                    }
+                if (i / k == 1) return j + 1;
+                if (!check[i / k]) {
+                    q.push({i / k, j + 1});
+                    check[i / k] = 1;
                 }
             }
         }
