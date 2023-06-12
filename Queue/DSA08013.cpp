7e85ea56d0330eac6544e59c7ade52678f4255a0
VanTienDesktop <pvantien203@gmail.com>
2023-06-12 16:44:36 +0700

    Di chuyển tránh vật cản

--- Queue/DSA08013.cpp
+++ Queue/DSA08013.cpp
@@ -1,64 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
-char a[101][101];
-int n, d[101][101];
-bool solve(int si, int sj, int ei, int ej) {
-    queue<pair<int, int>> q;
-    q.push({si, sj});
-    int i, j;
-    while (!q.empty()) {
+int solve(int si, int sj, int ei, int ej, int n, vector<char> a[]) {
+    queue<pair<pair<int, int>, int>> q;
+    int vs[n][n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) vs[i][j] = 0;
+    q.push({{si, sj}, 0});
+    vs[si][sj] = 1;
+    int i, j, k, d;
+    while (q.size()) {
         auto it = q.front(); q.pop();
-        i = it.first; j = it.second;
-        for (int k = i + 1; k < n; k++) {
-            if (k == si && j == sj) continue;
-            if (a[k][j] == 'X') break;
-            if (!d[k][j]) {
-                d[k][j] = d[i][j] + 1;
-                if (k == ei && j == ej) return true;
-                else q.push({k, j});
+        i = it.first.first; j = it.first.second;
+        d = it.second;
+        k = i + 1;
+        while (k < n && a[k][j] == '.') {
+            if (!vs[k][j]) {
+                if (k == ei && j == ej) return d + 1;
+                vs[k][j] = 1;
+                q.push({{k, j}, d + 1});
             }
+            k++;
         }
-        for (int k = i - 1; k >= 0; k--) {
-            if (k == si && j == sj) continue;
-            if (a[k][j] == 'X') break;
-            if (!d[k][j]) {
-                d[k][j] = d[i][j] + 1;
-                if (k == ei && j == ej) return true;
-                else q.push({k, j});
+        k = i - 1;
+        while (k >= 0 && a[k][j] == '.') {
+            if (!vs[k][j]) {
+                if (k == ei && j == ej) return d + 1;
+                vs[k][j] = 1;
+                q.push({{k, j}, d + 1});
             }
+            k--;
         }
-        for (int k = j + 1; k < n; k++) {
-            if (k == si && j == sj) continue;
-            if (a[i][k] == 'X') break;
-            if (!d[i][k]) {
-                d[i][k] = d[i][j] + 1;
-                if (i == ei && k == ej) return true;
-                else q.push({i, k});
+        k = j + 1;
+        while (k < n && a[i][k] == '.') {
+            if (!vs[i][k]) {
+                if (i == ei && k == ej) return d + 1;
+                vs[i][k] = 1;
+                q.push({{i, k}, d + 1});
             }
+            k++;
         }
-        for (int k = j - 1; k >= 0; k--) {
-            if (k == si && j == sj) continue;
-            if (a[i][k] == 'X') break;
-            if (!d[i][k]) {
-                d[i][k] = d[i][j] + 1;
-                if (i == ei && k == ej) return true;
-                else q.push({i, k});
+        k = j - 1;
+        while (k >= 0 && a[i][k] == '.') {
+            if (!vs[i][k]) {
+                if (i == ei && k == ej) return d + 1;
+                vs[i][k] = 1;
+                q.push({{i, k}, d + 1});
             }
+            k--;
         }
     }
-    return false;
+    return -1;
 }
 int main () {
     int t; cin >> t;
     while (t--) {
-        cin >> n;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) cin >> a[i][j];
-        memset(d, 0, sizeof(d));
+        int n; cin >> n;
+        vector<char> a[n]; char x;
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++) {
+                cin >> x; a[i].push_back(x);
+            }
         int i1, j1, i2, j2; cin >> i1 >> j1 >> i2 >> j2;
-        if (solve(i1, j1, i2, j2)) cout << d[i2][j2];
-        else cout << "-1";
-        cout << endl;
+        cout << solve(i1, j1, i2, j2, n, a) << '\n';
     }
     return 0;
 }
