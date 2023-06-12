ef81db5917ccf94862bf8061407f028b35e0f968
VanTienDesktop <pvantien203@gmail.com>
2023-06-12 09:10:08 +0700

    Kiểm tra biểu thức số học

--- Stack/DSA07003.cpp
+++ Stack/DSA07003.cpp
@@ -5,16 +5,16 @@ string check(vector<char> kt) {
     for (char x : kt) {
         if (x != ')') res.push(x); //chưa gặp dấu đóng ngoặc thì cứ push vào stack
         else {
-            //khi gặp dấu đóng ngoặc, ta chạy về trước cho đến khi gặp dấu mở ngoặc
+            //khi gặp dấu đóng ngoặc, chạy về trước cho đến khi gặp dấu mở ngoặc
             //loại bỏ tất cả các dấu +, -, * và / trên đường tìm kiếm, loại bỏ luôn mở ngoặc
             //Sẽ dư dấu ngoặc khi giữa 2 dấu mở và đóng không có dấu phép tính nào (flag = 0)
-            char sign = res.top(); res.pop();
-            int flag = 0;
+            char sign = res.top(); 
+            if (sign == '(') return "Yes";
             while (sign != '(') {
-                if (sign != '(') flag = 1;
-                sign = res.top(); res.pop();
+                res.pop();
+                sign = res.top();
             }
-            if (!flag) return "Yes";
+            res.pop();
         }  
     }
     return "No";
@@ -25,11 +25,9 @@ int main() {
     while (t--) {
         string s; getline(cin, s);
         vector<char> kt;
-        for (char x : s) {
-            if (x == '+' || x == '-' || x == '*' || x == '/' || x == '(' || x == ')') {
+        for (char x : s) 
+            if (x == '+' || x == '-' || x == '*' || x == '/' || x == '(' || x == ')') 
                 kt.push_back(x);
-            }
-        }
         cout << check(kt) << endl;
     }
 }
