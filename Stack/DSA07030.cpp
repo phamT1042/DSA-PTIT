2df389db3e29b6a0d5c4cdd4c6394dadab95b647
VanTienDesktop <pvantien203@gmail.com>
2023-06-12 10:58:44 +0700

    Hình chữ nhật 0 - 1

--- Stack/DSA07030.cpp
+++ Stack/DSA07030.cpp
@@ -5,46 +5,39 @@ int main() {
 	while (t--) {
 		int n, m; cin >> n >> m;
 		int a[n][m], cot[n][m];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				cin >> a[i][j];
-				cot[i][j] = a[i][j];
-			}
+		for (int i = 0; i < m; i++) {
+			cin >> a[0][i];
+			cot[0][i] = a[0][i];
 		}
 		for (int i = 1; i < n; i++) {
 			for (int j = 0; j < m; j++) {
+				cin >> a[i][j];
 				if (a[i][j]) cot[i][j] = cot[i - 1][j] + 1;
+				else cot[i][j] = 0;
 			}
 		}
-		int Smax = INT_MIN;
+		int Smax = INT_MIN, idx;
 		for (int i = 0; i < n; i++) {
-			for (int j = 1; j < m - 1; j++) {
-				if (cot[i][j] != 0) {
-					int l = j, r = j;
-					while (cot[i][l - 1] >= cot[i][j] && l > 0) l--;
-					while (cot[i][r + 1] >= cot[i][j] && r < m - 1) r++;
-					int S = (r - l + 1) * cot[i][j];
-					Smax = max(Smax, S);
+			stack<int> st;
+			st.push(0);
+			for (int j = 1; j < m; j++) {
+				while (cot[i][j] < cot[i][st.top()]) {
+					idx = st.top(); st.pop();
+					if (st.empty()) {
+						Smax = max(Smax, cot[i][idx] * j); 
+						break;
+					}
+					else Smax = max(Smax, cot[i][idx] * (j - st.top() - 1));
 				}
+				st.push(j);
+			}
+			while (st.size()) {
+				idx = st.top(); st.pop();
+				if (st.empty()) Smax = max(Smax, cot[i][idx] * m);
+				else Smax = max(Smax, cot[i][idx] * (m - st.top() - 1));
 			}
 		}
-		for (int i = 0; i < n; i++) {
-			int j = 0, r = 0;
-			if (cot[i][j] != 0) {
-				while (cot[i][r + 1] >= cot[i][j] && r < m - 1) r++;
-				int S = (r + 1) * cot[i][j];
-				Smax = max(Smax, S);
-			}
-		}
-		for (int i = 0; i < n; i++) {
-			int l = m - 1, j = m - 1;
-			if (cot[i][j] != 0) {
-				while (cot[i][l - 1] >= cot[i][j] && l > 0) l--;
-				int S = (j - l + 1) * cot[i][j];
-				Smax = max(Smax, S);
-			}
-		}
-		cout << Smax << endl;
+		cout << Smax << '\n';
 	}
 	return 0;
 }
