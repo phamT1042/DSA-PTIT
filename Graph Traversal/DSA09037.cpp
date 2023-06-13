6705ff90c3883c009fb61deb3ec77e470f778e17
VanTienDesktop <pvantien203@gmail.com>
2023-06-13 09:24:22 +0700

    Họp mặt

--- Graph Traversal/DSA09037.cpp
+++ Graph Traversal/DSA09037.cpp
@@ -2,45 +2,39 @@
 using namespace std;
 int visited[1001];
 vector<int> ke[1001];
-set<int> resPos, resPos_element;
+unordered_map<int, int> posCanGo;
 //Tạo danh sách các vị trí có thể đến của người đầu tiên
 void DFS_first (int u) {
     visited[u] = 1;
-    resPos.insert(u);
+    posCanGo[u] = 1;
     for (int v : ke[u]) {
         if (!visited[v]) DFS_first(v);
     }
 }
 //Tạo danh sách con các vị trí có thể đến từ người 2 trở đi
-void DFS (int u) {
+void DFS (int u, int i) {
     visited[u] = 1;
-    resPos_element.insert(u);
+    if (posCanGo[u] == i - 1) posCanGo[u] = i;
     for (int v : ke[u]) {
-        if (!visited[v]) DFS(v);
+        if (!visited[v]) DFS(v, i);
     }
 }
 int main () {
     int k, n, m; cin >> k >> n >> m;
-    int pos[k];
-    for (int &x : pos) cin >> x;
+    int pos[k + 1];
+    for (int i = 1; i <= k; i++) cin >> pos[i];
     while (m--) {
         int x, y; cin >> x >> y;
         ke[x].push_back(y);
     }
-    DFS_first(pos[0]);
-    for (int i = 1; i < k; i++) {
+    DFS_first(pos[1]);
+    for (int i = 2; i <= k; i++) {
         memset(visited, 0, sizeof(visited));
-        resPos_element.clear();
-        DFS(pos[i]);
-        vector<int> delete_Pos;
-        for (int x : resPos) {
-            //Loại các vị trí người 1 đến được nhưng người thứ 2 về sau không thể đến
-            if (resPos_element.find(x) == resPos_element.end()) {
-                delete_Pos.push_back(x);
-            }
-        }
-        for (int x : delete_Pos) resPos.erase(x);
+        DFS(pos[i], i);
     }
-    cout << resPos.size();
+    int res = 0;
+    for (auto it : posCanGo) 
+        if (it.second == k) res++;
+    cout << res;
     return 0;
 }
